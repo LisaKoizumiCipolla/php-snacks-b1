f6d07e2ba34f904485cc56e21c71bef54790d2f1
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-07-03 12:28:01 +0200

    Indentation adjustment

--- index.php
+++ index.php
@@ -8,68 +8,69 @@
     <title>Document</title>
 </head>
     <body>
-        <?php
+            <?php
 
-        $matches = [
-            [
-                'team1' => 'Acqua S.Bernardo Cantù',
-                'team2' => 'Oriora Pistoia',
-                'point_team_1' => 70,
-                'point_team_2' => 45
-            ],
-            [
-                'team1' => 'Fortitudo Pompea Bologna',
-                'team2' => 'Dolomiti Energia Trentino',
-                'point_team_1' => 82,
-                'point_team_2' => 83
-            ],
-            [
-                'team1' => 'Pallacanestro Trieste',
-                'team2' => 'Virtus Roma',
-                'point_team_1' => 72,
-                'point_team_2' => 33
-            ],
-            [
-                'team1' => 'Openjobmetis Varese',
-                'team2' => 'Germani Basket Brescia',
-                'point_team_1' => 101,
-                'point_team_2' => 99
-            ],
-            [
-                'team1' => 'Giggioletta Giggiona',
-                'team2' => 'Aquile Reali Unilaterali',
-                'point_team_1' => 121,
-                'point_team_2' => 111
-            ],
-            [
-                'team1' => 'Unicorni Vipiteno',
-                'team2' => 'Lampredotti Lampedusa',
-                'point_team_1' => 88,
-                'point_team_2' => 89
-            ],
-        ]; 
+                $matches = [
+                    [
+                        'team1' => 'Acqua S.Bernardo Cantù',
+                        'team2' => 'Oriora Pistoia',
+                        'point_team_1' => 70,
+                        'point_team_2' => 45
+                    ],
+                    [
+                        'team1' => 'Fortitudo Pompea Bologna',
+                        'team2' => 'Dolomiti Energia Trentino',
+                        'point_team_1' => 82,
+                        'point_team_2' => 83
+                    ],
+                    [
+                        'team1' => 'Pallacanestro Trieste',
+                        'team2' => 'Virtus Roma',
+                        'point_team_1' => 72,
+                        'point_team_2' => 33
+                    ],
+                    [
+                        'team1' => 'Openjobmetis Varese',
+                        'team2' => 'Germani Basket Brescia',
+                        'point_team_1' => 101,
+                        'point_team_2' => 99
+                    ],
+                    [
+                        'team1' => 'Giggioletta Giggiona',
+                        'team2' => 'Aquile Reali Unilaterali',
+                        'point_team_1' => 121,
+                        'point_team_2' => 111
+                    ],
+                    [
+                        'team1' => 'Unicorni Vipiteno',
+                        'team2' => 'Lampredotti Lampedusa',
+                        'point_team_1' => 88,
+                        'point_team_2' => 89
+                    ],
+                ]; 
 
-        foreach ($matches as $array)
-        { ?>
-            <?php 
-            {
-            ?>
-                <?php 
-                foreach ($array as $key => $game)
+                foreach ($matches as $array)
                 { ?>
 
-                    <p>
-                        <?php echo $key . ' ' . $game; ?>
-                    </p>
-                    
+                    <?php 
+                    {
+                    ?>
+                        <?php 
+                        foreach ($array as $key => $game)
+                        { ?>
+
+                            <p>
+                                <?php echo $key .  $game; ?>
+                            </p>
+                            
+                        <?php
+                        };
+                        ?>
+                <?php        
+                    }; 
+                ?>
             <?php
             };
-            ?>
-        <?php        
-            }; 
-         ?>
-         <?php
-        };
         ?>
     </body>
 </html>
