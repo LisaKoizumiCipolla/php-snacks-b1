502c6e1977dcc4de4364daeced740e3d795e96e7
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-07-04 13:08:18 +0200

    Post correction

--- snack4/index.php
+++ snack4/index.php
@@ -2,6 +2,16 @@
 
 $numbers = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8];
 
-$choosenOne = array_slice($numbers, 2 , 4);
+function numbersSlice ($array, $min, $max){
+    if ((count($array) < $max) || ($min > $max)){
+        return false;
+    } else {
+        $choosenOne = [];
+         for ($i=$min ;$i < $max + 1 ; $i++){
+            $choosenOne[] = $array[$i];
+        }
+        return $choosenOne;
+    }
+}
 
-var_dump($choosenOne);
+var_dump(numbersSlice($numbers, 0, 4));
